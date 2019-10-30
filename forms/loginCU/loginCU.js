afb8c6ed9263db1ffbf5fe31baf473cee4aa417c
Lauren <laurenermer@hotmail.com>
2019-10-30 15:09:16 -0500

    yeet

--- forms/loginCU/loginCU.js
+++ forms/loginCU/loginCU.js
@@ -1,3 +1,32 @@
 
 
+btnNextPage.onclick=function(){
+  ChangeForm(favFoods)
+}
 
+btnLogin.onclick=function(){
+  req1 = ""
+    req1 = Ajax("https://radlab.creighton.edu/appStudio/authLDAP.php", "POST", "j_username=" + inptNetID.value + "&j_password=" + inptPassword.value);
+    if (req1.status == 200) { //everything worked.
+        lblWelcome.style.display = "block"  // none to hide
+        lblResponse.style.display = "block"  // none to hide
+        lblResponse.value = "The authentication code is " + req1.responseText
+      if (req1.responseText = 1) {
+        ChangeForm(favFoods)
+        }
+      else {
+        lblResponse.value = "That login did not work. Please try again"
+     }
+        
+        //1 good 0 bad
+
+    } else {
+        //Handle that. 
+        lblResponse.style.display = "block"  // none to hide
+        lblResponse.value = "Error Connection Not Made: " + req1.status + " readystate " + req1.readyState + " status text " + req1.statusText;
+    }
+}
+
+
+  
+  
