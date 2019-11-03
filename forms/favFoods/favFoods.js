4b2fadd51311c154109254a658b491f7ddfd082a
Lauren <laurenermer@hotmail.com>
2019-11-03 17:02:38 -0600

    food

--- forms/favFoods/favFoods.js
+++ forms/favFoods/favFoods.js
@@ -1,1 +1,11 @@
 let goodFoods = ["pizza","apples","cookies","sushi","pasta","steak"]
+favFoods.onshow=function(){
+              lstgFood.clear()
+            for (i = 0; i <= goodFoods.length - 1; i++)
+                lstgFood.addItem(goodFoods[i])
+
+}
+
+lstgFood.onclick=function(){
+  lblFood.value = `You picked ${goodFoods[lstgFood.value]}- that's a great choice!`
+}
